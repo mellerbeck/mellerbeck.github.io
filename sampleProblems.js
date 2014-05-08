87d6eb8d8c12e944056be9046f05c423fdda07eb
mellerbeck <mellerbeck@gmail.com>
2014-05-08 14:30:48 -0700

    couple tweaks

--- sampleProblems.js
+++ sampleProblems.js
@@ -1,7 +1,7 @@
 var samples = {
 	AmeliaAddition: [
 		{key:'1 + 1', value:'2', altval1:'do', altval2:'q'},
-		{key:'2 + 2', value:'4', altval1:'door',altval2:'bob'},
+		{key:'2 + 2', value:'4', altval1:'door',altval2:'fort'},
 		{key:'2 + 1', value:'3', altval1:'tree',altval2:'bob'}
 	]
 };
