e23880949db5c228210ba53c0f0bef222161247e
mellerbeck <mellerbeck@gmail.com>
2014-05-09 16:35:34 -0700

    up

--- sampleProblems.js
+++ sampleProblems.js
@@ -2,7 +2,7 @@ var samples = {
 	AmeliaAddition: [
 		{key:'1 + 1', value:'2', altval1:'do', altval2:'q', altval3:'tube'},
 		{key:'2 + 2', value:'4', altval1:'door',altval2:'fort', altval3:'bob'},
-		{key:'2 + 1', value:'3', altval1:'tree',altval2:'bob', altval3:'free'}
+		{key:'2 + 1', value:'3', altval1:'tree',altval2:'bob', altval3:'free'},
 		{key:'3 + 3', value:'6', altval1:'six',altval2:'bob', altval3:'bob'}
 	]
 };
